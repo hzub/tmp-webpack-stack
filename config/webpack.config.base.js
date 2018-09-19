f55ed489def813230b4702808c26a90c49b91678
Hubert Zub <hubert.zub@softwarehut.com>
2018-09-19 10:08:55 +0200

    tidying up webpack config

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,5 +1,24 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+function getBaseLoaders() {
+  return [{
+    enforce: 'pre',
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: 'eslint-loader'
+  },
+    {
+      test: /\.jsx?$/,
+      loaders: [
+        'babel-loader',
+      ],
+      exclude: /node_modules/,
+    },];
+}
 function getStyleLoadersProduction() {
   return [{
     test: /\.scss$/,
@@ -58,6 +77,94 @@ function getStyleLoaders(isProduction) {
   return isProduction ? getStyleLoadersProduction() : getStyleLoadersDevelopment();
 }
 
+function getAssetLoaders() {
+  return [
+    {
+      test: /\.(png|jpe?g|gif)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 192,
+            mimetype: 'image/png',
+            name: 'images/[name].[ext]',
+          }
+        }
+      ],
+    },
+    {
+      test: /\.eot(\?v=\d+.\d+.\d+)?$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
+        }
+      ],
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            mimetype: 'application/font-woff',
+            name: 'fonts/[name].[ext]',
+          }
+        }
+      ],
+    },
+    {
+      test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            mimetype: 'application/octet-stream',
+            name: 'fonts/[name].[ext]',
+          }
+        }
+      ],
+    },
+    {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            mimetype: 'image/svg+xml',
+            name: 'images/[name].[ext]',
+          }
+        }
+      ],
+    },
+  ];
+}
+
+function getPlugins() {
+  return [
+    new HtmlWebpackPlugin({
+      template: 'index.ejs',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ];
+}
+
 module.exports = {
   getStyleLoaders,
+  getBaseLoaders,
+  getAssetLoaders,
+  getPlugins,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
 const { resolve } = require('path');
 
-const webpack = require('webpack');
-// const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 const configBase = require('./config/webpack.config.base');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -41,113 +34,14 @@ const config = {
 
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.jsx?$/,
-        loaders: [
-          'babel-loader',
-        ],
-        exclude: /node_modules/,
-      },
+      ...(configBase.getBaseLoaders(isProduction)),
       ...(configBase.getStyleLoaders(isProduction)),
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 192,
-              mimetype: 'image/png',
-              name: 'images/[name].[ext]',
-            }
-          }
-        ],
-      },
-      {
-        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]'
-            }
-          }
-        ],
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              mimetype: 'application/font-woff',
-              name: 'fonts/[name].[ext]',
-            }
-          }
-        ],
-      },
-      {
-        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              mimetype: 'application/octet-stream',
-              name: 'fonts/[name].[ext]',
-            }
-          }
-        ],
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-              mimetype: 'image/svg+xml',
-              name: 'images/[name].[ext]',
-            }
-          }
-        ],
-      },
+      ...(configBase.getAssetLoaders(isProduction)),
     ]
   },
 
   plugins: [
-    /*new webpack.LoaderOptionsPlugin({
-      test: /\.jsx?$/,
-      options: {
-        eslint: {
-          configFile: resolve(__dirname, '.eslintrc'),
-          cache: false,
-        }
-      },
-    }),*/
-    new HtmlWebpackPlugin({
-      template: `${__dirname}/src/index.ejs`,
-      filename: 'index.html',
-      inject: 'body',
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    // /*new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
-    // new CopyWebpackPlugin([{ from: 'vendors', to: 'vendors' }]),
-    new OpenBrowserPlugin({ url: 'http://localhost:8080' }),
-    new webpack.HotModuleReplacementPlugin(),
-
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+    ...(configBase.getPlugins(isProduction)),
   ],
 };
 
